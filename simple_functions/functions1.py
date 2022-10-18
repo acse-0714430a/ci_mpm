824a9c57218821d8d2ff08a41b3e0601da48a8dd
acse-0714430a <yc2122@ic.ac.uk>
2022-10-18 14:55:55 +0100

    Exercise 1

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -12,6 +12,7 @@ def my_sum(iterable):
 def factorial(n):
     return n * factorial(n-1) if n else 1
 
+@cache
 def sin(x):
-    import math
-    return math.sin(x)
+    coeff = (-1)**n/factorial(2*n+1)
+    return my_sum(coeff*(x**(2*n+1)))
